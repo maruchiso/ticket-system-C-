d7400546c176b044a2a9224afbbf8bd4923c7402
Marcin Szymczak <92090671+maruchiso@users.noreply.github.com>
2024-02-20 18:21:09 +0100

    Add_admin_panel

---
+++ panel_admin.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ticket_system
+{
+    public partial class WebForm6 : System.Web.UI.Page
+    {
+        string strcon = ConfigurationManager.ConnectionStrings["con_comp"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                try
+                {
+                    LoadTickets();
+                }
+                catch(Exception ex) 
+                {
+                    Response.Write("<script>alert('" + ex.Message + "');</script>");
+                }
+            }
+        }
+
+        protected void LoadTickets()
+        {
+            using (SqlConnection con = new SqlConnection(strcon))
+            {
+                if (con.State == System.Data.ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+
+                if (Session["User"] != null)
+                {
+                    using (SqlCommand cmd = new SqlCommand("SELECT * FROM Zgloszenia_User ORDER BY DataZgloszenia ASC;", con))
+                    {
+                        SqlDataReader reader = cmd.ExecuteReader();
+
+                        Zgloszenia.DataSource = reader;
+                        Zgloszenia.DataBind();
+                        reader.Close();
+                    }
+                }
+            }
+        }
+    }
+}
